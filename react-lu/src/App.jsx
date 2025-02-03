cd1abf0a1edb960c059fc569c1c91db13fd0431c
Atharva <athrudev@gmail.com>
2025-02-03 20:00:47 +0530

    completed the day 1 noes

--- react-lu/src/App.jsx
+++ react-lu/src/App.jsx
@@ -7,9 +7,9 @@ function App() {
   return (
     <>
      <h1>Hello everyone!</h1>
-     <Product name="Iphone 16"></Product>
-     <Product name="Samsung M15"></Product>
-     <Product name="Redmi S15"></Product>
+     <Product name="Iphone 16" price={80000}></Product>
+     <Product name="Samsung M15" price={70000}></Product>
+     <Product name="Redmi S15" price={45000}></Product>
     </>
   )
 }
--- react-lu/src/Product.jsx
+++ react-lu/src/Product.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Product = (props) => {
   return (
     <>
-    <div>Product name: {props.name}</div>
+    <h3>Product name: {props.name}</h3>
+    <p>price: {props.price}</p>
     </>
   )
 }
