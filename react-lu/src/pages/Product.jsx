a5e6b88ede327a97053ed4ee3b787320c52c0ca1
Atharva <athrudev@gmail.com>
2025-02-05 19:02:47 +0530

    created an map function to show the array of object's

--- react-lu/src/pages/Product.jsx
+++ react-lu/src/pages/Product.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Product = () => {
+
+
+  let[avengers,setAvengers]=useState([
+
+    {name:"Thor",age:1500},
+    {name:"Iron man",age:50},
+    {name:"Captain America",age:150},
+
+  ])
+
+
   return (
+
+    <>
+    
     <div className='Products'>Product</div>
+    
+    {
+      avengers.map((ele)=>{
+        return(
+          <div className='box'>
+            <h1>{ele.name}</h1>
+            <h3>{ele.age}</h3>
+          </div>
+          )
+      })
+
+    }
+
+    </>
   )
 }
 
